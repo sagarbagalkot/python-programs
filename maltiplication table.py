9382814dd747fbdf1b02c3fba8b8a656c4fa596e
Sagar Bagalkot <bagalkotsagar@gmail.com>
2024-09-30 15:31:56 +0530

    multiplication table

---
+++ maltiplication table.py
@@ -0,0 +1,8 @@
+#maltiplication table
+print("U15IG22S0054")
+n=int(input("enter n value"))
+for j in range(1,11):
+    for i in range(2,n+1):
+        x=i*j
+        print(x, end=" ")
+    print("\n")
